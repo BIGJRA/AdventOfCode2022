68e92e2c4f94b5540ee5b4b640b0baca605eb6e7
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2022-12-19 13:07:55 -0800

    finished with favor high resources pruning

--- 2022_19.py
+++ 2022_19.py
@@ -1,9 +1,6 @@
 from aocd import lines, submit
 import re
-from functools import lru_cache
 
-lines = '''Blueprint 1:Each ore robot costs 4 ore.Each clay robot costs 2 ore.Each obsidian robot costs 3 ore and 14 clay.Each geode robot costs 2 ore and 7 obsidian.
-Blueprint 2:Each ore robot costs 2 ore.Each clay robot costs 3 ore.Each obsidian robot costs 3 ore and 8 clay.Each geode robot costs 3 ore and 12 obsidian.'''.splitlines()
 
 def solve(part_no):
     def get_blueprints(lines):
@@ -11,49 +8,65 @@ def solve(part_no):
         for line in lines:
             nums = re.findall(r'\d+', line)
             blueprints.append(tuple([(int(nums[1]), 0, 0, 0),
-                               (int(nums[2]), 0, 0, 0),
-                               (int(nums[3]), int(nums[4]), 0, 0),
-                               (int(nums[5]), 0, int(nums[6]), 0)]))
+                                     (int(nums[2]), 0, 0, 0),
+                                     (int(nums[3]), int(nums[4]), 0, 0),
+                                     (int(nums[5]), 0, int(nums[6]), 0)]))
         return tuple(blueprints)
 
-    @lru_cache()
-    def play(blueprint, min_left, resources, robots):
-        if min_left == 0:
-            # print(resources, robots)
-            return resources[3]
-        ret = 0
-        for spent, added_robots in generate_buy_options(blueprint, resources):
-            ret = max(ret, play(blueprint, min_left - 1,
-                 tuple([resources[i] + robots[i] - spent[i] for i in range(4)]),
-                 tuple([robots[i] + added_robots[i] for i in range(4)])))
-        return ret
+    def play(blueprint, num_minutes):
+        max_geodes = 0
 
-    @lru_cache()
-    def generate_buy_options(blueprint, resources):
-        spent = (0,0,0,0)
-        robots = (0,0,0,0)
-        yield spent, robots
-        for idx, robot_type in enumerate(blueprint):
-            if all([robot_type[i] <= resources[i] for i in range(4)]):
-                nxt_spent = tuple([spent[i] + robot_type[i] for i in range(4)])
-                nxt_robots = tuple([robots[i] + (i == idx) for i in range(4)])
-                yield nxt_spent, nxt_robots
+        queue = [(0, (1, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0))]
+        # States: (minutes_passed, robots, on_hand_resources, all_mined_resources)
+        max_minutes_passed = 0
 
+        while len(queue) > 0:
+            minutes_passed, robots, resources, mined_resources = queue.pop(0)
+
+            # Prune once per minute
+            if minutes_passed > max_minutes_passed:
+                # Sorting function just massively favors those that have found more advanced resources
+                queue.sort(key=lambda state: sum([state[3][i] * 24 ** i for i in range(4)]), reverse=True)
+                queue = queue[:keep]
+                max_minutes_passed = minutes_passed
+
+            # Iteration is over - update max geodes
+            if minutes_passed == num_minutes:
+                max_geodes = max(max_geodes, mined_resources[3])
+                continue
+
+            new_resources = tuple([resources[i] + robots[i] for i in range(4)])
+            mined_resources = tuple([mined_resources[i] + robots[i] for i in range(4)])
+
+            queue.append((minutes_passed + 1, robots, new_resources, mined_resources))
+            for idx, robot_type in enumerate(blueprint):
+                if all([robot_type[i] <= resources[i] for i in range(4)]):
+                    nxt_resources = tuple([new_resources[i] - robot_type[i] for i in range(4)])
+                    nxt_robots = tuple([robots[i] + (i == idx) for i in range(4)])
+                    queue.append((minutes_passed + 1, nxt_robots, nxt_resources, mined_resources))
+        return max_geodes
+
+    # Too lazy to use logic to prune branches, I just take those with the most 'advanced' resources so far.
+    # Can modify how many to keep - too few = less chance of correctness, too many = overtime.
+    # Here I just upped it until sample input passed :)
+    keep = 8000
     p = get_blueprints(lines)
-    m = 0
-    for blueprint in p:
-        #print (blueprint)
-        # score = 0
-        sc = play(blueprint, 24, (0, 0, 0, 0), (1, 0, 0, 0))
-        #print (sc)
-        m += sc
-    return m
+    if part_no == 2:
+        p = p[:3]
+    ans = part_no - 1  # P1: 0, P2: 1. for add/mult. Over-engineering???? :)
+    for idx, blueprint in enumerate(p):
+        mined_geodes = play(blueprint, {1: 24, 2: 32}[part_no])
+        if part_no == 1:
+            ans += (idx + 1) * mined_geodes
+        if part_no == 2:
+            ans *= mined_geodes
+    return ans
 
 
 p1 = solve(1)
 print(p1)
 # submit(p1)
 
-# p2 = solve(2)
-# print(p2)
+p2 = solve(2)
+print(p2)
 # submit(p2)
