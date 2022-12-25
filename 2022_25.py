c70758cf10ff6bad927299a49994db8c66f9ecfe
BIGJRA <47573834+BIGJRA@users.noreply.github.com>
2022-12-25 00:48:53 -0800

    all done with AOC

--- 2022_25.py
+++ 2022_25.py
@@ -1,12 +1,23 @@
 from aocd import lines, submit
 
-def solve(part_no):
-    return part_no
+def solve():
 
-p1 = solve(1)
+    def dec_to_snafu(dec):
+        chars = []
+        while dec > 0:
+            mod_adj = (dec + 2) % 5
+            chars.append(['=','-','0','1','2'][mod_adj])
+            dec = (dec + 2) // 5
+        return ''.join(chars[::-1])
+
+    def snafu_to_dec(snafu):
+        dec = 0
+        for pwr, char in enumerate(snafu[::-1]):
+            dec += ((['=','-','0','1','2'].index(char) - 2) * 5 ** pwr)
+        return dec
+
+    return dec_to_snafu(sum([snafu_to_dec(snafu) for snafu in lines]))
+
+p1 = solve()
 print(p1)
 # submit(p1)
-
-p2 = solve(2)
-print(p2)
-# submit(p2)
